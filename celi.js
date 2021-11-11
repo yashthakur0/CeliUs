6209b7fa40c6fea60eb66c243206ea747ec62111
Yash THakur <yash.thakur@ceryxdigital.com>
2021-11-11 16:17:19 +0530

    Some Small Changes

---
+++ celi.js
@@ -0,0 +1,17 @@
+
+ (function ( $ ) {
+ 
+    $.fn.celi = function( options ) {
+        let value = parseInt($(this).val());
+        return  value ;    
+        // let cuberoot = Math.cbrt(value)
+        // let cuberoot = Math.random(value)
+        // let cuberoot = Math.ceil(value)
+        // let cuberoot = Math.sign(value)
+        // let cuberoot = Math.sign(value)
+        // return cuberoot;
+    };
+  }( jQuery ));
+  
+  
+      
